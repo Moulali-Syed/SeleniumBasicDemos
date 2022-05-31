d6be85e404c38d37e5dadcb6b4d1b4dcffe26df6
Moulali Syed <syedmoulali1997@gmail.com>
2022-05-31 07:05:30 +0530

    ExtentReportsBasicDemo

---
+++ FrameworkBasics/ExtentReports/src/test/java/ExtentReportDemo.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportDemo {
+
+	ExtentReports extent;
+
+	@BeforeTest
+	public void config() {
+
+		// get the project path dynamically using
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		// ExtentSparkReporter is responsible for creating html file and put some
+		// configuration inside it
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+
+		// ExtentReports responsible for all the execution
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);// pass ExtentSparkReporter object
+		extent.setSystemInfo("Tester", "Moulali");
+	}
+
+	@Test
+	public void demo() {
+		
+		ExtentTest test =  extent.createTest("demo");
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com");
+
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+		
+		test.fail("Result donot match");
+		extent.flush();
+	}
+}
