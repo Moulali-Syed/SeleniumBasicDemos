97d761ebb83d836ae127b43da4f479482899d703
Moulali Syed <syedmoulali1997@gmail.com>
2022-07-06 16:41:38 +0530

    java

---
+++ JavaAndExcel/JavaSmartProgramming/src/java2/NArray.java
@@ -0,0 +1,46 @@
+package java2;
+
+public class NArray {
+
+}
+/*
+ suppose we have so many employees in a company
+ 
+ int emp_id =101;
+ int emp_id =102;
+ ..............
+ 
+ int emp_id =1000;
+ 
+ instead we can store all emp_id in a single variable
+ 
+ Array is collection of homogeneous data
+ 
+ int emp_id[] = new int[1000];
+ 
+ accessing elements in array:
+ can access with index
+ 
+  new keyword is used for object creation , array is object 
+  array super class is Object
+  
+  array occupies memory in Heap
+  
+  index value start from 0,.....
+  
+  advantages:
+  speed of array is fast compared to primitive variable declaration because there is
+  not internal conversion of wrapper class
+  
+  disadvantages:
+  size is fixed , cant increase it run time
+  wastage of memory is high
+  only similar datatype values will be stored
+  if we remove a single element in a array from middle , all elements next to it
+  should move one position ahead , thus decrease array performance
+ String emp_name[] = new String[1000];
+ 
+ 
+ 
+ 
+ */
---
+++ JavaAndExcel/JavaSmartProgramming/src/java2/P2DArray.java
@@ -0,0 +1,44 @@
+package java2;
+
+public class P2DArray {
+
+}
+
+/*
+ *2d array:
+ *declaration - 
+ 			int[][] a;
+ 			int[] []a;
+ 			int a[][];
+  			int[] a[];
+  			
+  			
+  			int[] a,b;//a,b are 1D arrays
+  			int a[],b; // a is 1D array , b is int variable
+  			
+  			 int[][] a,b; //a,b are 2D arrays
+  			 int[] a[],b; // a is 2d array , b is 1D array
+  			 int[] a[],b[]; //a , b are 2D arrays
+  			 int[][] a,b[]; //a is 2D , b is 3D arrays
+  			 int[][] a,[]b; //a is 2D array , but for 2nd varibale b we cannot provide
+  			 				//dimension before , it results in compile time error
+  			 		
+  			 		
+  	int[][] a;//declaration
+  	a = new int[2][3]; //creation
+  	
+  	if number of columns are equal , then it is called matrix array
+  	
+  	
+  	Jagged array:
+  	int[][] b = new int[2][];
+  	declaring 2nd dimension size is optional
+  		b[0] = new int[3];	
+  		b[1] = new int[2];
+  		
+  	Initialization:
+  			int[][] a;//declare
+  			a = new int[2][3];//creation
+  			
+  			 				 
+ */
